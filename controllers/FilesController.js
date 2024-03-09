a244187d5772d77fad38a67e40882689eac41917
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-09 20:48:59 +0300

    :sparkles:feat:Post upload route

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,44 +1,9 @@
-import { tmpdir } from 'os';
-import { promisify } from 'util';
-import { v4 as uuidv4 } from 'uuid';
-import { mkdir, writeFile } from 'fs';
-import { join as joinPath } from 'path';
-import { ObjectId } from 'mongodb';
-import dbClient from '../utils/db';
+const { ObjectId } = require('mongodb');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const dbClient = require('../utils/db');
 
-const VALID_FILE_TYPES = {
-  folder: 'folder',
-  file: 'file',
-  image: 'image',
-};
-const ROOT_FOLDER_ID = 0;
-const DEFAULT_ROOT_FOLDER = 'files_manager';
-const mkDirAsync = promisify(mkdir);
-const writeFileAsync = promisify(writeFile);
-const NULL_ID = Buffer.alloc(24, '0').toString('utf-8');
-
-const isValidId = (id) => {
-  const size = 24;
-  let i = 0;
-  const charRanges = [
-    [48, 57], // 0 - 9
-    [97, 102], // a - f
-    [65, 70], // A - F
-  ];
-  if (typeof id !== 'string' || id.length !== size) {
-    return false;
-  }
-  while (i < size) {
-    const c = id[i];
-    const code = c.charCodeAt(0);
-
-    if (!charRanges.some((range) => code >= range[0] && code <= range[1])) {
-      return false;
-    }
-    i += 1;
-  }
-  return true;
-};
 class FilesController {
   /**
    * Uploads a file.
@@ -46,81 +11,77 @@ class FilesController {
    * @param {Response} res The Express response object.
    */
   static async postUpload(req, res) {
-    const { user } = req;
-    const {
-      name, type, parentId, isPublic, data,
-    } = req.body;
-    const base64Data = data || '';
+    try {
+      const { user } = req;
 
-    if (!name) {
-      return res.status(400).json({ error: 'Name is required' });
-    }
+      // Specifiy the file information
+      const acceptedTypes = ['folder', 'file', 'image'];
 
-    if (!type || !Object.values(VALID_FILE_TYPES).includes(type)) {
-      return res.status(400).json({ error: 'Invalid file type' });
-    }
+      const {
+        name, type, parentId = 0, isPublic = false, data,
+      } = req.body;
 
-    if (!data && type !== VALID_FILE_TYPES.folder) {
-      return res.status(400).json({ error: 'File data is required' });
-    }
+      if (!name) {
+        return res.status(400).json({ error: 'Missing name' });
+      }
+      if (!type || !acceptedTypes.includes(type)) {
+        return res.status(400).json({ error: 'Missing type' });
+      }
+      if (!data && type !== 'folder') {
+        return res.status(400).json({ error: 'Missing data' });
+      }
 
-    if (parentId !== ROOT_FOLDER_ID && parentId !== ROOT_FOLDER_ID.toString()) {
+      if (parentId !== 0) {
+        const filesCollection = await dbClient.filesCollection();
+        const parentFile = filesCollection.findOne({ _id: ObjectId(parentId) });
+
+        if (!parentFile) {
+          return res.status(400).json({ error: 'Parent not found' });
+        }
+        if (parentFile.type !== 'folder') {
+          return res.status(400).json({ error: 'Parent is not a folder' });
+        }
+      }
+
+      // Add the user ID to the document
+      //  Prepare file document
+      const fileDocument = {
+        userId: ObjectId(user._id),
+        name,
+        type,
+        isPublic,
+        parentId: parentId === 0 ? parentId : ObjectId(parentId),
+      };
+
+      if (type === 'folder') {
+        const filesCollection = await dbClient.filesCollection();
+        const newFile = await filesCollection.insertOne(fileDocument);
+
+        return res.status(201).json(newFile.ops[0]);
+      }
+      if (type === 'file' || type === 'image') {
+        // Save file to disk
+        const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+
+        if (!fs.existsSync(folderPath)) {
+          fs.mkdirSync(folderPath, { recursive: true });
+        }
+        const fileUUIDV4 = uuidv4();
+        const localPath = path.join(folderPath, fileUUIDV4);
+
+        // Decode base64 data and write to file
+        fs.writeFileSync(localPath, Buffer.from(data, 'base64'));
+      }
+
+      // Add the new file document in the collection files
       const filesCollection = await dbClient.filesCollection();
-      const file = await filesCollection.findOne({
-        _id: ObjectId(isValidId(parentId) ? parentId : NULL_ID),
-      });
+      const newFile = await filesCollection.insertOne(fileDocument);
 
-      if (!file) {
-        return res.status(400).json({ error: 'Parent folder not found' });
-      }
-
-      if (file.type !== VALID_FILE_TYPES.folder) {
-        return res.status(400).json({ error: 'Parent is not a folder' });
-      }
+      return res.status(201).json(newFile.ops[0]);
+    } catch (err) {
+      console.log(`Error: ${err}`);
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    const userId = user._id.toString();
-    const folderPath = process.env.FOLDER_PATH
-      ? process.env.FOLDER_PATH.trim()
-      : null;
-    const baseDir = folderPath && folderPath.length > 0
-      ? folderPath
-      : joinPath(tmpdir(), DEFAULT_ROOT_FOLDER);
-    // default baseDir == '/tmp/files_manager'
-    // or (on Windows) '%USERPROFILE%/AppData/Local/Temp/files_manager';
-
-    await mkDirAsync(baseDir, { recursive: true });
-
-    const newFile = {
-      userId: ObjectId(userId),
-      name,
-      type,
-      isPublic: Boolean(isPublic),
-      parentId:
-        parentId === ROOT_FOLDER_ID || parentId === ROOT_FOLDER_ID.toString()
-          ? ROOT_FOLDER_ID
-          : ObjectId(parentId),
-    };
-
-    if (type !== VALID_FILE_TYPES.folder) {
-      const localPath = joinPath(baseDir, uuidv4());
-      await writeFileAsync(localPath, Buffer.from(base64Data, 'base64'));
-      newFile.localPath = localPath;
-    }
-    const filesCollection = await dbClient.filesCollection();
-    const { insertedId } = await filesCollection.insertOne(newFile);
-
-    return res.status(201).json({
-      id: insertedId.toString(),
-      userId,
-      name,
-      type,
-      isPublic: isPublic || 'false',
-      parentId:
-        parentId === ROOT_FOLDER_ID || parentId === ROOT_FOLDER_ID.toString()
-          ? 0
-          : parentId,
-    });
   }
 }
 
