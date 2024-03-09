d134fb9c03c25bc2044d93e7cb4283ddfd703785
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-09 13:24:07 +0300

    :sparkles:feat:Create user route

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,5 @@
-import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 
 /**
  * Controller for handling user-related operations.
@@ -37,37 +35,6 @@ class UsersController {
     const userId = insertionInfo.insertedId.toString();
 
     res.status(201).json({ email, id: userId });
-  }
-
-  /**
-   * Handles retrieving of a user in session.
-   * @param {Request} req The request object.
-   * @param {Response} res The response object.
-   * @returns {void}
-   */
-  static async getMe(req, res) {
-    try {
-      const token = req.headers['x-token'];
-      if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      const userId = await redisClient.get(`auth_${token}`);
-      if (!userId) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      const usersCollection = await dbClient.usersCollection();
-      const user = await usersCollection.findOne({ _id: ObjectId(userId) });
-      if (!user) {
-        return res.status(401).json({ error: 'Unauthorized' });
-      }
-
-      res.status(200).json({ id: user._id.toString(), email: user.email });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
   }
 }
 
