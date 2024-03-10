431ee50a2ae9306b21c3f117082516f2154fc2f0
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-10 22:23:02 +0300

    :sparkles:feat:Get file(s) route

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -158,6 +158,8 @@ class FilesController {
           filesFilter.parentId = ROOT_FOLDER_ID.toString();
         } else if (ObjectId.isValid(parentId)) {
           filesFilter.parentId = ObjectId(parentId);
+        } else {
+          filesFilter.parentId = ObjectId(NULL_ID);
         }
       }
 
@@ -165,7 +167,7 @@ class FilesController {
       const files = await filesCollection
         .aggregate([
           { $match: filesFilter },
-          { $sort: { _id: -1 } },
+          { $sort: { _id: 1 } },
           { $skip: page * MAX_FILES_PER_PAGE },
           { $limit: MAX_FILES_PER_PAGE },
           {
