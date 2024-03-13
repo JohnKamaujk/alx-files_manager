a3eb93cef6f79d2ad90ccdcc26a13ea20d4ec891
JohnKamaujk <johnjkkamau@gmail.com>
2024-03-13 11:58:17 +0300

    :test_tube:tests

--- tests/utils/db.test.js
+++ tests/utils/db.test.js
@@ -4,6 +4,7 @@ import dbClient from '../../utils/db';
 describe('+ DBClient utility', () => {
   before(function (done) {
     this.timeout(10000);
+    setTimeout(done, 4000);
     Promise.all([dbClient.usersCollection(), dbClient.filesCollection()])
       .then(([usersCollection, filesCollection]) => {
         Promise.all([
